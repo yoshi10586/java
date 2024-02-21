8b1ac8c5426d887babefbd31807923c58b7d8403
yoshi10586 <160204003+yoshi10586@users.noreply.github.com>
2024-02-21 17:50:39 +0900

    Update Person.java
    JPA, バリデーションの学習

--- SampleBootApp1/src/main/java/com/example/sample1spp/Person.java
+++ SampleBootApp1/src/main/java/com/example/sample1spp/Person.java
@@ -6,26 +6,56 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name="people")
 public class Person {
 
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	@Column
+//	@NotNull
+//	private long id;
+//	
+//	@Column(length = 50, nullable = false)
+//	@NotBlank(message="名前は書かないとダメ！")
+//	private String name;
+//	
+//	@Column(length = 200, nullable = true)
+//	@Email(message="メールアドレスを教えて♡")
+//	private String mail;
+//	
+//	@Column(nullable = true)
+//	@Min(value=0, message="いやいや、マイナスの歳ってないでしょ？")
+//	@Max(value=200, message="200歳以上って、魔女ですか？")
+//	private Integer age;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column
+	@NotNull
 	private long id;
 	
 	@Column(length = 50, nullable = false)
+	@NotBlank
 	private String name;
 	
 	@Column(length = 200, nullable = true)
+	@Email
 	private String mail;
 	
 	@Column(nullable = true)
+	@Min(0)
+	@Max(200)
 	private Integer age;
 	
 	@Column(nullable = true)
+//	@Phone(onlyNumber=true)
 	private String memo;
 
 	public long getId() {
