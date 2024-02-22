88854f6bb7814106e0ae56a02fa66e63aa7254f3
yoshi10586 <160204003+yoshi10586@users.noreply.github.com>
2024-02-22 17:32:00 +0900

    EntityManager、JPQL、エンティティの連携の学習

--- SampleBootApp1/src/main/java/com/example/sample1spp/PersonDAO.java
+++ SampleBootApp1/src/main/java/com/example/sample1spp/PersonDAO.java
@@ -5,4 +5,8 @@ import java.util.List;
 
 public interface PersonDAO <T> extends Serializable {
 	public List<T> getAll();
+	public T findById(Long id);
+	public List<T> findByName(String name);
+	public List<T> find(String fstr);
+	public List<T> findByAge(int min, int max);
 }
