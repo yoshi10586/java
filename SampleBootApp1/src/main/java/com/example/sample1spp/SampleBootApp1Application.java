45992ce767807e1b6a5f29a17053c07376a41d0d
yoshi10586 <160204003+yoshi10586@users.noreply.github.com>
2024-02-22 17:35:46 +0900

    EntityManager、JPQL、エンティティの連携の学習

--- SampleBootApp1/src/main/java/com/example/sample1spp/SampleBootApp1Application.java
+++ SampleBootApp1/src/main/java/com/example/sample1spp/SampleBootApp1Application.java
@@ -1,17 +1,25 @@
-package com.example.sample1spp;
+package com.example.sample1app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.sample1app.repositories.PersonRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 /////////  Default  //////////
-@SpringBootApplication
-public class SampleBootApp1Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SampleBootApp1Application.class, args);
-	}
-
-}
+//@SpringBootApplication
+//public class SampleBootApp1Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SampleBootApp1Application.class, args);
+//	}
+//
+//}
 
 ///////////  変数化  //////////
 //@SpringBootApplication
@@ -230,3 +238,40 @@ public class SampleBootApp1Application {
 //	}
 //
 //}
+@SpringBootApplication
+public class SampleBootApp1Application  implements CommandLineRunner{
+	@PersistenceContext
+	private EntityManager entityManager;
+	@Autowired
+	PersonRepository repository;
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
+		app.setBannerMode(Mode.OFF);
+		app.run(args);
+	}
+	@Override
+	public void run(String[] args) {
+		init();
+		Person person = entityManager.find(Person.class, 1);
+		System.out.println(("+--------------------------------------"));
+		System.out.println(person.getName() + ", " + person.getMail() + ", " +person.getAge());
+		System.out.println(("+--------------------------------------"));
+	}
+	public void init() {
+		Person p1 = new Person();
+		p1.setName("taro");
+		p1.setAge(39);
+		p1.setMail("taro@yamada");
+		repository.saveAndFlush(p1);
+		Person p2 = new Person();
+		p2.setName("hanako");
+		p2.setAge(28);
+		p2.setMail("hanako@flower");
+		repository.saveAndFlush(p2);
+		Person p3 = new Person();
+		p3.setName("sachiko");
+		p3.setAge(17);
+		p3.setMail("sachiko@happy");
+		repository.saveAndFlush(p3);
+	}
+}
