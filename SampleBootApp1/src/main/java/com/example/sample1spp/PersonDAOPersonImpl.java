5736ee59ab66fc430e2ef88010c18808d74f62aa
yoshi10586 <160204003+yoshi10586@users.noreply.github.com>
2024-02-22 17:32:50 +0900

    EntityManager、JPQL、エンティティの連携の学習

--- SampleBootApp1/src/main/java/com/example/sample1spp/PersonDAOPersonImpl.java
+++ SampleBootApp1/src/main/java/com/example/sample1spp/PersonDAOPersonImpl.java
@@ -24,4 +24,79 @@ public class PersonDAOPersonImpl implements PersonDAO<Person>{
 		entityManager.close();
 		return list;
 	}
+	@Override
+	public Person findById(Long id) {
+		return (Person)entityManager.createQuery("from Person where id ="
+				+ id).getSingleResult();
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Person> findByName(String name) {
+		return (List<Person>)entityManager.createQuery("from Person where name = '"
+				+ name + "'").getResultList();
+	}
+//	@Override
+//	public List<Person> find(String fstr) {
+//		List<Person> list = null;
+//		String qstr = "from Person where id = :fstr";
+//		Query query = entityManager.createQuery(qstr)
+//				.setParameter("fstr", Long.parseLong(fstr));
+//		list = query.getResultList();
+//		return list;
+//	}
+//	@SuppressWarnings("unchecked")
+//	@Override
+//	public List<Person> find(String fstr) {
+//		List<Person> list = null;
+//		String qstr = "from Person where id = :fid or name like :fname or mail like :fmail";
+//		Long fid = 0L;
+//		try {
+//			fid = Long.parseLong(fstr);
+//		} catch (NumberFormatException e) {
+//			e.printStackTrace();
+//		}
+//		Query query = entityManager.createQuery(qstr)
+//				.setParameter("fid", fid)
+//				.setParameter("fname", "%" + fstr + "%")
+//				.setParameter("fmail", fstr + "%@%");
+//		list = query.getResultList();
+//		return list;
+//	}
+//	@SuppressWarnings("unchecked")
+//	@Override
+//	public List<Person> find(String fstr) {
+//		List<Person> list = null;
+//		String qstr = "from Person where id = ?1 or name like ?2 or mail like ?3";
+//		Long fid = 0L;
+//		try {
+//			fid = Long.parseLong(fstr);
+//		} catch (NumberFormatException e) {
+//			e.printStackTrace();
+//		}
+//		Query query = entityManager.createQuery(qstr)
+//				.setParameter(1, fid)
+//				.setParameter(2, "%" + fstr + "%")
+//				.setParameter(3, fstr + "%@%");
+//		list = query.getResultList();
+//		return list;
+//	}
+	@Override
+	public List<Person> find(String fstr) {
+		List<Person> list = null;
+		String qstr = "findWithName";
+		Query query = entityManager.createNamedQuery(qstr)
+				.setParameter("fname", "%" + fstr + "%");
+		list = query.getResultList();
+		return list;
+	}
+	@Override
+	public List<Person> findByAge(int min, int max) {
+		List<Person> list = null;
+		String qstr = "findByAge";
+		Query query = entityManager.createNamedQuery(qstr)
+				.setParameter("min", min)
+				.setParameter("max", max);
+		list = query.getResultList();
+		return list;
+	}
 }
